768c90f83f8bd1799114618b9da1f69ce3c9315b
Boaz Frankel <>
2020-09-20 15:33:18 +0300

    changing the Rscript: (1) can be run from command line using Rscript with input and output positional arguments supplied (a usage print was added) (2) a 'V_FAMILY' column is added to the input csv as well as the output csv which only has a 'V_FAMILY' output column

--- V_family_labels.R
+++ V_family_labels.R
@@ -2,13 +2,23 @@
 library(alakazam)
 library(dplyr)
 
-# Subset example data
-path <-'/media/miri-o/Documents/filtered_data_sets/'
-db <- read.csv(paste(path,'FLU_data_012119_HCV_model_Jan19_2019_FILTERED_DATA.csv', sep = ""))
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)<2 || length(args)>3) {
+  stop("Usage: V_family_lables.R <input csv file> <output file>", call.=FALSE)
+}
+input_file = args[1]
+output_file = args[2]
 
-a <- getFamily(db$V_CALL, first = FALSE, collapse = TRUE)
-db$V_FAMILY <- a
-write.table(a, file = paste(path, "FLU_data_HCV_model_Jan21_2019_labels.csv", sep = ""), append = FALSE, quote = FALSE, sep = "\t",
+print(paste('loading input from', input_file))
+db <- read.csv(input_file, sep='\t')
+db['V_FAMILY'] <- getFamily(db$V_CALL, first = FALSE, collapse = TRUE)
+write.table(db, file = input_file, append = FALSE, quote = FALSE, sep = '\t',
                           eol = "\n", na = "NA", dec = ".", row.names = FALSE,
                           col.names = TRUE, qmethod = c("escape", "double"),
                           fileEncoding = "")
+write.table(db %>% select(V_FAMILY), file = output_file, append = FALSE, quote = FALSE, sep = '\t',
+                          eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+                          col.names = TRUE, qmethod = c("escape", "double"),
+                          fileEncoding = "")
+print(paste('labels file saved to', output_file))
+
