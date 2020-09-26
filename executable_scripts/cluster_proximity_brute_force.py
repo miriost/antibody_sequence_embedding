cbcf07152fa69732b4eb8bc5b898c2b90309df2d
Boaz Frankel <>
2020-09-26 18:38:00 +0300

    split job to smaller portions because of memory limitation

--- executable_scripts/cluster_proximity_brute_force.py
+++ executable_scripts/cluster_proximity_brute_force.py
@@ -68,7 +68,6 @@ def init_logger():
     logger.addHandler(file_handler)
 
 
-
 def main():
     init_logger()
     
@@ -118,43 +117,60 @@ def main():
     print('-----------------------')
     output_file_name = args.output_description + '.csv'
     if args.perform_NN:
-        knn_map = cluster(args.vectors_file_path, args.output_folder_path, output_file_name, cluster_size=100,
-                          cpus=args.cpus)
-
-
-def get_distance_map(data, cpus=2):
-    distances_map = pairwise_distances(X=data, metric='euclidean', n_jobs=cpus)
-    return distances_map
+        cluster(args.vectors_file_path, args.output_folder_path, output_file_name, cluster_size=100, cpus=args.cpus)
 
 
 @ray.remote
-def get_knn(data, k):
-    return np.argpartition(data, k, axis=1)[:, 0:k]
+def build_distance_and_knn_maps(data, sub_row_range, k, cpus=2):
+    t0 = time.time()
+    distances_map = pairwise_distances(X=data[sub_row_range[0]:sub_row_range[1]], Y=data, metric='euclidean', n_jobs=cpus)
+    print("build_distance_map: creating sub map took {}".format(time.time() - t0))
+
+    t0 = time.time()
+    knn_map = np.argpartition(distances_map, k, axis=1)[:, 0:k]
+    print("build_knn_map: creating sub map took {}".format(time.time() - t0))
+
+    return distances_map[:, 0:k], knn_map
 
 
-def get_knn_map(distance_map, cluster_size, cpus=2):
-    # parallel getting the proximity map
-    step = distance_map.shape[0]/cpus
-    ranges = [[round(step*i), round(step*(i+1))] for i in range(cpus)]
+def build_sub_map(data, major_row_range, cluster_size, cpus=2):
+    step = (major_row_range[1]-major_row_range[0]) / cpus
+    ranges = [[round(step * i), round(step * (i + 1))] for i in range(cpus)]
     results_ids = []
-    for x in ranges:
-        results_ids += [get_knn.remote(distance_map[x[0]:x[1], :], cluster_size+1)]
-    proximity_map = np.zeros(shape=[distance_map.shape[0], cluster_size+1], dtype=int)
-    for i, x in enumerate(ranges):
-        res = ray.get(results_ids[i])
-        proximity_map[x[0]:x[1], :] = res
 
-    return pd.DataFrame(proximity_map)
+    knn_map = np.zeros(shape=[major_row_range[1]-major_row_range[0], cluster_size+1], dtype=int)
+    distances_map = np.zeros(shape=[major_row_range[1]-major_row_range[0], cluster_size+1])
+    for minor_row_range in ranges:
+        sub_row_range = [major_row_range[0] + minor_row_range[0], major_row_range[0] + minor_row_range[1]]
+        results_ids += [build_distance_and_knn_maps.remote(data, sub_row_range, k=cluster_size+1)]
+    for i, minor_row_range in enumerate(ranges):
+        sub_distances_map, sub_knn_map = ray.get(results_ids[i])
+        distances_map[minor_row_range[0]:minor_row_range[1], :] = sub_distances_map
+        knn_map[minor_row_range[0]:minor_row_range[1], :] = sub_knn_map
+
+    return distances_map, knn_map
 
 
-def test_build_dist_and_knn_maps(dim_one=100, dim_two=10, cluster_size=3, cpus=2):
-    df = pd.DataFrame(np.random.rand(dim_one, dim_two))
+def build_maps(data, cluster_size, cpus=2, step=20000):
+    knn_map = np.zeros(shape=[data.shape[0], cluster_size+1], dtype=int)
+    distances_map = np.zeros(shape=[data.shape[0], cluster_size+1])
+    ranges = [[round(step*i), round(step*(i+1))] for i in range(round(data.shape[0]/step))]
+
+    for major_row_range in ranges:
+        t0 = time.time()
+        sub_distances_map, sub_knn_map = build_sub_map(data, major_row_range, cluster_size, cpus)
+        print("build_maps: creating sub map (step {}) took {}".format(major_row_range[1], time.time()-t0))
+        distances_map[major_row_range[0]:major_row_range[1], :] = sub_distances_map
+        knn_map[major_row_range[0]:major_row_range[1], :] = sub_knn_map
+
+    return distances_map, knn_map
+
+
+def test_build_dist_and_knn_maps(dim_one=1000, dim_two=10, cluster_size=5, cpus=2):
+    data = np.random.rand(dim_one, dim_two)
     t0 = time.time()
-    distance_map = get_distance_map(df, cpus)
-    print("distance map completed after {}".format(time.time() - t0))
-    t0 = time.time()
-    knn_map = get_knn_map(distance_map, cluster_size, cpus)
-    print("knn map completed after {}".format(time.time() - t0))
+    distance_map, knn_map = build_maps(data, cluster_size, cpus=cpus, step=10)
+    print("building maps completed after {}".format(time.time() - t0))
 
 
 def cluster(data_file, output_file_path, output_file_name, cluster_size=100, cpus=2):
@@ -162,15 +178,12 @@ def cluster(data_file, output_file_path, output_file_name, cluster_size=100, cpu
     vectors = read_vector_data(data_file)
 
     t0 = time.time()
-    distance_map = get_distance_map(vectors, cpus)
-    print("distance map completed after {}".format(time.time() - t0))
-
-    t0 = time.time()
-    knn_map = get_knn_map(distance_map, cluster_size, cpus)
-    print("knn map completed after {}".format(time.time() - t0))
+    distance_map, knn_map = build_maps(vectors, cluster_size, cpus)
+    print("cluster: building maps completed after {}".format(time.time() - t0))
 
     write_output_file(os.path.join(output_file_path, 'NN_' + output_file_name), knn_map)
-    write_output_file(os.path.join(output_file_path, 'Distances_' + output_file_name), distance_map)
+    write_output_file(os.path.join(output_file_path, 'Distances_' + output_file_name),
+                      distance_map.loc[:, 0:cluster_size])
     print(str(datetime.now()) + ' | finished clustering, files saved to: ')
     logger.info(str(datetime.now()) + ' | finished clustering, files saved to: ')
     logger.info(os.path.join(output_file_path, 'NN_' + output_file_name))
