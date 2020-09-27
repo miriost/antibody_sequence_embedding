009d05f00834fbebe8b6979f79ff424eb88b99ee
Boaz Frankel <>
2020-09-28 02:23:31 +0300

    change vector files argument to optional - can sample only data file

--- sample_file.py
+++ sample_file.py
@@ -12,6 +12,7 @@ import numpy as np
 import sys, argparse
 from datetime import datetime
 
+
 def main(argv):
 
     parser = argparse.ArgumentParser(
@@ -24,7 +25,8 @@ def main(argv):
     parser.add_argument('--output_data_file', type=str, help='output filtered data file path')
     parser.add_argument('--input_vector_file', type=str, help='input vectors file path')
     parser.add_argument('--output_vector_file', type=str, help='output vectors file path')
-    parser.add_argument('--subject_field', type=str, default='FILENAME', help='name of the column which identifies the subjects')
+    parser.add_argument('--subject_field', type=str, default='FILENAME', help='name of the column which identifies '
+                                                                              'the subjects')
 
     args = parser.parse_args()
     max_num_smaples_per_subject = args.max_samples
@@ -36,14 +38,16 @@ def main(argv):
     Subject_field = args.subject_field
 
     Input_data_file = pd.read_csv(Input_filtered_data_file_path, sep='\t')
-    Input_vectors_file = pd.read_csv(Input_vector_file_path)
+    if Input_vector_file_path is not None:
+        Input_vectors_file = pd.read_csv(Input_vector_file_path)
 
     by_subject = Input_data_file.groupby([Subject_field])
     sub_num = 0
     positive_subjects = 0
 
-    output_data_df = pd.DataFrame(columns = list(Input_data_file.columns))
-    output_vectors_df = pd.DataFrame(columns = list(Input_vectors_file.columns))
+    output_data_df = pd.DataFrame(columns=list(Input_data_file.columns))
+    if Input_vector_file_path is not None:
+        output_vectors_df = pd.DataFrame(columns=list(Input_vectors_file.columns))
 
     np.random.seed(0)
     for subject, frame in by_subject:
@@ -63,7 +67,8 @@ def main(argv):
             positive_subjects += 1
             sampled_frame_indexes = np.random.choice(frame.index, min_num_samples_per_subject, replace=False)
             output_data_df = output_data_df.append(Input_data_file.iloc[sampled_frame_indexes])
-            output_vectors_df = output_vectors_df.append(Input_vectors_file.iloc[sampled_frame_indexes])
+            if Input_vector_file_path is not None:
+                output_vectors_df = output_vectors_df.append(Input_vectors_file.iloc[sampled_frame_indexes])
             print('Subject added.')
 
     print('----- Summary -----')
@@ -72,8 +77,10 @@ def main(argv):
 
     output_data_df.to_csv(Output_filtered_data_file_path, sep='\t', index=False)
     print('output data set saved to: ', Output_filtered_data_file_path)
-    output_vectors_df.to_csv(Output_vector_file_path, index=False)
-    print('output vectors set saved to: ', Output_vector_file_path)
+
+    if Input_vector_file_path is not None:
+        output_vectors_df.to_csv(Output_vector_file_path, index=False)
+        print('output vectors set saved to: ', Output_vector_file_path)
 
 
 if __name__ == "__main__":
