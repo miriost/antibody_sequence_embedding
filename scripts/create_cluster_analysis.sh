b4bbe812d57a522581ad6c22add663cd3e34b91c
boazfr@gmail.com <boazfr@ig03.lnx.biu.ac.il>
2020-10-06 16:05:53 +0300

    fix syntax bug

--- scripts/create_cluster_analysis.sh
+++ scripts/create_cluster_analysis.sh
@@ -21,8 +21,13 @@ done
 for fold in $(seq ${lower} 1 ${upper}) ; do
 	fold_dir=FOLD${fold}
 	# loop cluster size
-	for cs in seq(100 10 130); do
+	for cs in $(seq 100 10 130); do
 		output_dir=${fold_dir}/cs_${cs}
-		python -u ~/antibody_sequence_embedding/executable_scripts/cluster_proximity_brute_force.py -d ${fold_dir}/FILTERED_DATA_TRAIN.tab -v ${fold_dir}/VECTORS_TRAIN.csv --perform_NN=True --perform_results_analysis=True -of ${output_dir} -od cs_${cs} -cs ${cs} -tm 21474836480 --cpus=12 --step=10000 -id FILENAME 2>&1 | tee -a ${output_dir}/cs_${cs}_cluster_proximity_brute_force.log.txt
+		mkdir -p ${output_dir}
+		if [ -f ${output_dir}/feautre_list.csv ] ; then
+			# already has a feature_list.csv file - skipping
+			continue
+		fi
+		python -u ~/antibody_sequence_embedding/executable_scripts/cluster_proximity_brute_force.py -d ${fold_dir}/FILTERED_DATA_TRAIN.tab -v ${fold_dir}/VECTORS_TRAIN.csv --perform_NN=True --perform_results_analysis=True -of ${output_dir} -od cs_${cs} -cs ${cs} -tm 11474836480 --cpus=12 --step=10000 -id FILENAME 2>&1 | tee -a ${output_dir}/cs_${cs}_cluster_proximity_brute_force.log.txt
 	done
 done 
