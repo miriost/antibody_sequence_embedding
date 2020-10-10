045cf00112be71ba402669797dfc939a1464e0b5
boazfr@gmail.com <boazfr@ig03.lnx.biu.ac.il>
2020-10-10 12:10:04 +0300

    change arguments to scripts - explict lists instead of ranges

--- scripts/merge_classification_res.sh
+++ scripts/merge_classification_res.sh
@@ -2,27 +2,50 @@
 
 trap "exit" INT
 
-# loop folds
-for fold in 0 1 2 3 ; do
+usage="USAGE: merge_classification_res.sh -f [list of fold numbers] -c [list of cluster sizes] -s [list of segnificant levels] -m [list of min subjects] -o [classification res file name]"
+folds=$(seq 0 1 39)
+cluster_sizes=$(seq 100 10 130)
+segnificant_levels=$(seq 60 2 74)
+min_subjects=$(seq 8 1 14)
+classification_file=classification_res.csv
+while getopts "hf:c:s:m:o:" opt; do
+	case ${opt} in
+		h ) echo ${usage} ; exit 1
+      			;;
+    		f ) folds=${OPTARG}
+      			;;
+		c ) cluster_sizes=${OPTARG}
+			;;
+    		s ) segnificant_levels=${OPTARG}
+		     	;;
+		m ) min_subjects=${OPTARG}
+			;;
+		o ) classification_file=${OPTARG}
+			;;
+		\? ) echo ${usage}; exit 1
+      			;;
+	esac
+done
+
+# reset file if exists
+echo "" > all_${classification_file}
+# loop all folds
+for fold in ${folds} ; do
 	fold_dir=FOLD${fold}
 	# loop cluster size
-	for cs in seq(100 10 130); do
+	for cs in ${cluster_sizes}; do
 		cs_dir=${fold_dir}/cs_${cs}
 		# loop segnificant level
-		for seg_level in seq(60 2 74) ; do
+		for seg_level in ${segnificant_levels} ; do
 			sig=$(echo "scale=2;${seg_level}/100" | bc)
 			# loop min subjects
-			for min_subj in seq(8 1 14); do 
+			for min_subj in ${min_subjects}; do 
 				output_dir=${cs_dir}/seg_level_${seg_level}_min_subj_${min_subj}
-				if ! [ -f ${output_dir}/classification_res.csv ]; then
+				if ! [ -f ${output_dir}/${classification_file} ]; then
 					continue
 				fi
-				echo "merging ${output_dir}/classification_res.csv"
-				if ! [ -f all_classification_res.csv ]; then
-					cp ${output_dir}/classification_res.csv all_classification_res.csv
-		       		else
-					tail -n+2 ${output_dir}/classification_res.csv >> all_classification_res.csv 	
-				fi
+				echo "merging ${output_dir}/${classification_file}"
+				tail -n+2 ${output_dir}/${classification_file} >> all_${classification_file}	
 			done		
 		done
 	done
--- scripts/run_classification.sh
+++ scripts/run_classification.sh
@@ -2,21 +2,45 @@
 
 trap "exit" INT
 
+usage="USAGE: create_feature_list.sh -f [list of fold numbers] -c [list of cluster sizes] -s [list of segnificant levels] -m [list of min subjects] -o [output file name]"
+folds=$(seq 0 1 39)
+cluster_sizes=$(seq 100 10 130)
+segnificant_levels=$(seq 60 2 74)
+min_subjects=$(seq 8 1 14)
+output_file=classification_res.csv
+while getopts "hf:c:s:m:o:" opt; do
+	case ${opt} in
+		h ) echo ${usage} ; exit 1
+      			;;
+    		f ) folds=${OPTARG}
+      			;;
+		c ) cluster_sizes=${OPTARG}
+			;;
+    		s ) segnificant_levels=${OPTARG}
+		     	;;
+		m ) min_subjects=${OPTARG}
+			;;
+		o ) output_file=${OPTARG}
+			;;
+		\? ) echo ${usage}; exit 1
+      			;;
+	esac
+done
 
 # loop folds
-for fold in $(seq 0 1 39) ; do
+for fold in ${folds} ; do
 	fold_dir=FOLD${fold}
 	# loop cluster size
-	for cs in $(seq 100 10 130); do
+	for cs in ${cluster_sizes}; do
 		cs_dir=${fold_dir}/cs_${cs}
 		# loop segnificant level
-		for seg_level in $(seq 60 2 74) ; do
+		for seg_level in ${segnificant_levels} ; do
 			sig=$(echo "scale=2;${seg_level}/100" | bc)
 			# loop min subjects
-			for min_subj in $(seq 8 1 14); do 
+			for min_subj in ${min_subjects}; do 
 				output_dir=${cs_dir}/seg_level_${seg_level}_min_subj_${min_subj}
 				# runt the classification
-				python ~/antibody_sequence_embedding/executable_scripts/classify_no_splitting.py --train_file ${output_dir}/train_feature_table.csv  --test_file ${output_dir}/test_feature_table.csv --col_names="fold,cluster_size,segnificant" --col_values="${fold},${cs},${sig}" --output_file ${output_dir}/classification_res.csv -M all
+				python -u ~/antibody_sequence_embedding/executable_scripts/classify_no_splitting.py --train_file ${output_dir}/train_feature_table.csv  --test_file ${output_dir}/test_feature_table.csv --col_names="fold,cluster_size,segnificant" --col_values="${fold},${cs},${sig}" --output_file ${output_dir}/${output_file} -M all 2>&1 | tee ${output_dir}/classifiy_no_splitting.log.txt
 			done # min subjects loop
 		done # segnificant level loop
 	done # cluster size loop
