3f0729cdaa2a8f24f640c4340965e5ef93afb120
boazfr@gmail.com <boazfr@bcr.lnx.biu.ac.il>
2020-10-25 01:42:11 +0200

    adjust scrit to work without vecotr file - vector is a column in the data file

--- executable_scripts/build_cluster_proximty_feature_list.py
+++ executable_scripts/build_cluster_proximty_feature_list.py
@@ -7,7 +7,7 @@ Created on Thu Apr 23 10:18:13 2020
 """
 Cluster proximity to feature table
 
-Feture selection
+Feature selection
 ================
 Start by feature selection according to the following parameters:
     1. min_number_of_subjects- how many subjects are in each cluster
@@ -17,68 +17,79 @@ Start by feature selection according to the following parameters:
 """
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.spatial.distance import euclidean
+import json
 import sys, argparse
 import os
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--analysis_file_path',
-                        help='the filtered data file path')
-    parser.add_argument('-ds', '--distances_file_path',
+    parser.add_argument('--data_file_path',
+                        help='the tsv file path')
+    parser.add_argument('--analysis_file_path',
+                        help='the analysis file path')
+    parser.add_argument('--distances_file_path',
                         help='the distances file path')
-    parser.add_argument('-v', '--vectors_file_path',
-                        help='the vectors file path')
-    parser.add_argument('-of', '--output_folder_path',
+    parser.add_argument('--vector_column',
+                        help='the name of the column with the vector')
+    parser.add_argument('--output_folder_path',
                         help='Output folder for the 3 output files - Nearest neighbors file, distances file, and '
                              'results analysis file')
-    parser.add_argument('-od', '--output_description',
+    parser.add_argument('--output_description',
                         help='description to use inside output file names')
-    parser.add_argument('-l', '--label_freq_col', default='HC',
+    parser.add_argument('--label_freq_col', default='HC',
                         help='name of the column in the analysis file with the label frequency')
-    parser.add_argument('-S','--significance', help='minimal significance score for cluster selection',
+    parser.add_argument('--significance', help='minimal significance score for cluster selection',
                         default=0.6, type=float)
-    parser.add_argument('-m','--min_subjects', help='minimal number of subjects for cluster selection',
+    parser.add_argument('--min_subjects', help='minimal number of subjects for cluster selection',
                         default=10, type=int)
     args = parser.parse_args()
-    
+
+    if not(os.path.isfile(args.data_file_path)):
+        print('data file error, make sure file path exists\nExiting...')
+        sys.exit(1)
+
     if not(os.path.isfile(args.analysis_file_path)):
         print('analysis file error, make sure file path exists\nExiting...')
         sys.exit(1)
-        date
+
     if not(os.path.isfile(args.distances_file_path)):
         print('distances file error, make sure file path exists\nExiting...')
         sys.exit(1)
-            
-    if not(os.path.isfile(args.vectors_file_path)):
-        print('vectors file error, make sure file path exists\nExiting...')
-        sys.exit(1) 
+
+    if args.vector_column is None:
+        print("Missing vector_column argument\nExisting...")
+        sys.exit(1)
 
     # load files    
-    inputfile = pd.read_csv(args.analysis_file_path)
-    vectors_file = pd.read_csv(args.vectors_file_path)
+    data_file = pd.read_csv(args.data_file_path, sep='\t')
+    if args.vector_column not in data_file.columns:
+        print("{} is not in data file columns: {}\nExisting...".format(args.vector_column, data_file.columns))
+        sys.exit(1)
+
+    vectors = np.array(data_file[args.vector_column].apply(lambda x: json.loads(x)).to_list())
+    vectors = pd.DataFrame(vectors, columns=list(range(vectors.shape[1])))
     distance_file = pd.read_csv(args.distances_file_path)
-    
+    analysis_file = pd.read_csv(args.analysis_file_path)
+
     #load parameters
     min_number_of_subjects = args.min_subjects
     min_significance = args.significance
 
     #calculate significance score    
-    inputfile['SIGNIFICANCE'] = [max(a, 1-a) for a in inputfile[args.label_freq_col]]
-    sorted_df = inputfile.sort_values(by=['SIGNIFICANCE'], ascending=False)
+    analysis_file['significance'] = [max(a, 1-a) for a in analysis_file[args.label_freq_col]]
+    sorted_df = analysis_file.sort_values(by=['significance'], ascending=False)
     
     #processing
     number_of_features_neto = 0
     number_of_features_bruto = 0
     # A list with the same length as the data, nan on all cells except the chosen features, which will have 1
-    selected_feature_indexes = [np.nan] * len(inputfile)
+    selected_feature_indexes = [np.nan] * len(analysis_file)
     # A list with the same length as the data, nan on all cells except the chosen points, which will have the index of
     # the original cell which created the cluster
-    neighbors_feature_index = [np.nan] * len(inputfile)
+    neighbors_feature_index = [np.nan] * len(analysis_file)
     for idx, val in sorted_df.iterrows():
-        if val['SIGNIFICANCE'] > min_significance and val['how_many_subjects'] > min_number_of_subjects:
+        if val['significance'] > min_significance and val['how_many_subjects'] > min_number_of_subjects:
             number_of_features_bruto += 1
             if np.isnan(neighbors_feature_index[idx]):
                 number_of_features_neto += 1
@@ -94,7 +105,7 @@ def main():
     # ====
     # build a feature list file, each raw contains feature center, and maximal radius
     c = ['feature_index', 'max_distance']
-    c.extend(vectors_file.columns)
+    c.extend(vectors.columns)
     print(c)
     selected_features = np.nonzero(~np.isnan(selected_feature_indexes))[0]
     print('selected features: ', selected_features)
@@ -106,7 +117,7 @@ def main():
     features_df = pd.DataFrame(0, index=list(range(0, number_of_features_neto)), columns=c)
     features_df['feature_index'] = selected_features
     features_df['max_distance'] = distance_file.iloc[selected_features].max(axis=1).values
-    features_df.iloc[:, 2:] = vectors_file.iloc[selected_features].values
+    features_df.iloc[:, 2:] = vectors.iloc[selected_features].values
 
     features_df.to_csv(os.path.join(args.output_folder_path, args.output_description + '.csv'), index=False)
     print('file saved to ', os.path.join(args.output_folder_path, args.output_description + '.csv'))
@@ -114,3 +125,4 @@ def main():
     
 if __name__ == '__main__':
     main()
+
