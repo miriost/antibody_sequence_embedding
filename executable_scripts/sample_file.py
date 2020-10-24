b271d94faa153f1f208d6dfd270f4be0966cb1f8
boazfr@gmail.com <boazfr@bcr.lnx.biu.ac.il>
2020-10-24 22:39:07 +0300

    adjust sample_file to work without a vectors file

--- executable_scripts/sample_file.py
+++ executable_scripts/sample_file.py
@@ -5,83 +5,78 @@ Created on Thu Apr  9 14:57:21 2020
 @author: mirio
 """
 
-# sample file 
-
 import pandas as pd
 import numpy as np
-import sys, argparse
-from datetime import datetime
+import argparse
+import os
 
 
-def main(argv):
+def main():
 
     parser = argparse.ArgumentParser(
-        description='''sample sequences by subjects from embedded vectors for the clustering/classification stage ''',
-        epilog="""All's well that ends well.""")
-    parser.add_argument('--max_samples', type=int, help='max number of sampled sequences per subject')
+        description='sample sequences by subjects from input file',
+        epilog="All's well that ends well.")
     parser.add_argument('--min_samples', type=int, help='min number of sampled sequences per subject. Default is 10k.',
                         default=10000)
     parser.add_argument('--input_data_file', type=str, help='input filtered data file path')
-    parser.add_argument('--output_data_file', type=str, help='output filtered data file path')
-    parser.add_argument('--input_vector_file', type=str, help='input vectors file path')
-    parser.add_argument('--output_vector_file', type=str, help='output vectors file path')
-    parser.add_argument('--subject_field', type=str, default='FILENAME', help='name of the column which identifies '
-                                                                              'the subjects')
+    parser.add_argument('--seed', type=str, help='seed for the random sampling', default=0)
+    parser.add_argument('--output_data_file', help='output filtered data file path', type=str)
+    parser.add_argument('--subject_field', help='name of the column which identifies the subjects '
+                                                '(default=repertoire.subject_id)', default='repertoire.subject_id')
+    parser.add_argument('--exclude_dup_column', help='Filter subject level duplication by this '
+                                                     'column (default=junction_aa)', default='junction_aa')
 
     args = parser.parse_args()
-    max_num_smaples_per_subject = args.max_samples
-    min_num_samples_per_subject = args.min_samples
-    Input_filtered_data_file_path = args.input_data_file
-    Output_filtered_data_file_path = args.output_data_file
-    Input_vector_file_path = args.input_vector_file
-    Output_vector_file_path = args.output_vector_file
-    Subject_field = args.subject_field
 
-    Input_data_file = pd.read_csv(Input_filtered_data_file_path, sep='\t')
-    if Input_vector_file_path is not None:
-        Input_vectors_file = pd.read_csv(Input_vector_file_path)
+    input_data_file = pd.read_csv(args.input_data_file, sep='\t')
+    # sort file before sampling to get consisted sampling in different runs (if input include same sequences
+    # but not necessarily sorted)
+    input_data_file.sort_index(inplace=True)
 
-    by_subject = Input_data_file.groupby([Subject_field])
+    by_subject = input_data_file.groupby([args.subject_field])
     sub_num = 0
     positive_subjects = 0
 
-    output_data_df = pd.DataFrame(columns=list(Input_data_file.columns))
-    if Input_vector_file_path is not None:
-        output_vectors_df = pd.DataFrame(columns=list(Input_vectors_file.columns))
+    np.random.seed(args.seed)
 
-    np.random.seed(0)
+    df_list = []
     for subject, frame in by_subject:
         sub_num += 1
         print("------------------------")
         print(f"Analysing {subject!r} {sub_num!r}")
-        dateTimeObj = datetime.now()
-        timeObj = dateTimeObj.time()
-        print(timeObj)
         print('Original number of sequences: ', len(frame), end="\n\n")
         # sample subject without replacement and save to list
-        if max_num_smaples_per_subject is not None and len(frame) >= max_num_smaples_per_subject:
-            print('High number of sequences, subject removed.\n')
-        if len(frame) < min_num_samples_per_subject:
-            print('Lower number of sequences, subject removed.\n')
-        else:
-            positive_subjects += 1
-            sampled_frame_indexes = np.random.choice(frame.index, min_num_samples_per_subject, replace=False)
-            output_data_df = output_data_df.append(Input_data_file.iloc[sampled_frame_indexes])
-            if Input_vector_file_path is not None:
-                output_vectors_df = output_vectors_df.append(Input_vectors_file.iloc[sampled_frame_indexes])
-            print('Subject added.')
+        tmp_df = frame.drop_duplicates(subset=[args.exclude_dup_column])
+        if len(tmp_df) < args.min_samples:
+            print('Lower number of sequences {}, subject removed.\n'.format(len(tmp_df)))
+            continue 
+        positive_subjects += 1
+        sampled_frame_indexes = np.random.choice(tmp_df.index, args.min_samples, replace=False)
+        df_list += [input_data_file.iloc[sampled_frame_indexes]]
+        print('Subject added.')
+
+    if len(df_list) == 0:
+        print("no sujbect passed filter")
+        return
+
+    output_data_df = pd.concat(df_list)
 
     print('----- Summary -----')
     print(f'Analysed {sub_num!r} subjects')
-    print(f'Chose {positive_subjects!r} subjects with <= {max_num_smaples_per_subject} number of sequences')
+    print(f'Chose {positive_subjects!r} subjects with >= {args.min_samples} number of sequences')
 
-    output_data_df.to_csv(Output_filtered_data_file_path, sep='\t', index=False)
-    print('output data set saved to: ', Output_filtered_data_file_path)
+    if args.output_data_file is None:
+        file_name = os.path.basename(args.input_data_file).split(".tsv")[0]
+        dir_name = os.path.dirname(args.input_data_file)
+        output_file_name = os.path.join(dir_name, 
+                file_name + '_sampled_n' + str(args.min_samples) + '_seed' + str(args.seed) + '.tsv')
+    else:
+        output_file_name = args.output_data_file
 
-    if Input_vector_file_path is not None:
-        output_vectors_df.to_csv(Output_vector_file_path, index=False)
-        print('output vectors set saved to: ', Output_vector_file_path)
+    output_data_df.to_csv(output_file_name, sep='\t', index=False)
+    print('output data set saved to: ', output_file_name)
 
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main()
+
