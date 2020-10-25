8d650ecfd45b30c2709bbe40ad1c6c97f124a494
boazfr@gmail.com <boazfr@bcr.lnx.biu.ac.il>
2020-10-26 00:43:43 +0200

    add 2 new scripts for the pre clustering and clustering piplines

---
+++ scripts/clustring_pipline.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+trap "exit" INT
+
+usage="USAGE: clustring_pipline.sh -v <vector_column> -f [folds] -c [cluster_sizes] -s [significance] -m [min_subjects] -w [work_dir]"
+help="Build clusters and feature tables for train/test folds.
+-v VECTOR_COLUMN - Mandatory, name of the column in the tsv file with the embedded vector.
+-f FOLDS - Optional, space separated list of folds numbers. Deafult is 0..9.
+-c CLUSTER_SIZES - Optional, space separated list of cluster sizes. Deafult is 80 100 120.
+-s SEGNIFICANCE - Optional, space separated list of min segnificance precentages for the feature (cluster) selection. Default is 60 63 66.
+-m MIN_SUBJECTS - Optional, space separated list of min subjects for the feature (cluster) selection. Default is 10.
+-w WORK_DIR - Optional, the folds root directory where the folds are. Default is \"./\".
+"
+folds=$(seq 0 1 9)
+cluster_sizes=$(seq 80 20 120)
+significance_levels=$(seq 60 3 66)
+min_subjects=10
+vector_column=""
+work_dir=./
+
+while getopts "hf:c:s:m::v:w:" opt; do
+	case ${opt} in
+		h ) echo "${usage}" ; echo "${help}"; exit 1
+      			;;
+    		f ) folds=${OPTARG}
+      			;;
+		c ) cluster_sizes=${OPTARG}
+			;;
+    		s ) significance_levels=${OPTARG}
+		     	;;
+		m ) min_subjects=${OPTARG}
+			;;
+		v) vector_column=${OPTARG}
+			;;
+		w) work_dir=${OPTARG}
+			;;
+		\? ) echo ${usage}; echo "cluster_pipline.sh -h for additional help"; exit 1
+      			;;
+	esac
+done
+
+# change to the working directory
+cd ${work_dir}
+
+# validate arguments
+if [ -z "${vector_column}" ]; then
+	echo "${usage}"
+	echo "cluster_pipline.sh -h for additional help"
+	exit -1
+fi
+
+# loop folds
+for fold in ${folds} ; do
+	echo "Fold ${fold}"; echo ""
+	fold_dir=FOLD${fold}
+	
+	# loop cluster size
+	for cs in ${cluster_sizes}; do
+		echo "Cluster size ${cs}"; echo ""
+		cs_dir=${fold_dir}/cs_${cs}
+		mkdir -p ${cs_dir}
+		if [ -f ${cs_dir}/NN_cs_${cs}.csv ] ; then
+			echo "${cs_dir}/NN_cs_${cs}.csv already exists, skipping KNN search."
+		else
+			# search K nearest neighbors
+			echo "Starting KNN search and analysis..."
+			eval python -u ~/antibody_sequence_embedding/executable_scripts/cluster_proximity_brute_force.py --data_file_path ${fold_dir}/*_TRAIN_*.tsv --perform_NN=True --perform_results_analysis=True --output_folder_path ${cs_dir} --vector_column ${vector_column} --output_description cs_${cs} --cluster_size ${cs} --cpus=12 --step=10000 --id repertoire.repertoire_name 2>&1 | tee -a ${cs_dir}/cs_${cs}_cluster_proximity_brute_force.log.txt
+		fi
+		if [ -f ${cs_dir}/cs_${cd}_analysis.csv ] ; then
+			echo "${cs_dir}/cs_${cd}_analysis.csv already exists, skipping KNN analysis."
+		else
+			# analyze K nearest neighbors
+			echo "Starting KNN analysis..."
+			eval python -u ~/antibody_sequence_embedding/executable_scripts/cluster_proximity_brute_force.py --data_file_path ${fold_dir}/*_TRAIN_*.tsv --NN_file_path=${output_dir}/NN_cs_${cs}.csv --perform_NN=False --perform_results_analysis=True --output_folder_path ${cs_dir} --vector_column ${vector_column} --output_description cs_${cs} --cluster_size ${cs} --thread_memory 11474836480 --cpus=12 --step=10000 --id repertoire.repertoire_name 2>&1 | tee -a ${cs_dir}/cs_${cs}_cluster_proximity_brute_force.log.txt
+		fi
+
+		# loop significance level
+		for sig_level in ${significance_levels} ; do
+			sig=$(echo "scale=2;${sig_level}/100" | bc)
+			echo "Significance ${sig}"; echo ""
+			
+			# loop min subjects
+			for min_subj in ${min_subjects}; do 
+				echo "Min subjects ${min_subj}"; echo ""
+				output_dir=${cs_dir}/sig_level_${sig_level}_min_subj_${min_subj}
+				mkdir -p ${output_dir}
+				if [ -f ${output_dir}/feature_list.csv ] ; then
+					echo "${output_dir}/feature_list.csv already exists, skipping building feature list." 
+				else	
+					# create feature list
+					echo "Building feature list..."
+					python -u ~/antibody_sequence_embedding/executable_scripts/build_cluster_proximty_feature_list.py --data_file_path ${fold_dir}/*_TRAIN_*.tsv --analysis_file_path ${cs_dir}/cs_${cs}_analysis.csv --distances_file_path ${cs_dir}/Distances_cs_${cs}.csv --vector_column ${vector_column} --output_folder ${output_dir} --output_description feature_list --label_freq_col Healthy --significance ${sig} --min_subjects ${min_subj} 2>&1 | tee ${output_dir}/build_cluster_proximity_feature_list.log.txt
+				fi
+
+				if [ -f ${output_dir}/train_feature_table.csv ] ; then
+					echo "${output_dir}/train_feature_table.csv already exists, skipping building train feature table."
+				else
+					echo "Building train feature table..."
+					# create train feature table
+					python -u ~/antibody_sequence_embedding/executable_scripts/build_cluster_proximty_feature_table_with_labels.py --features_list ${output_dir}/feature_list.csv --data_file_path ${fold_dir}/*_TRAIN_*.tsv --vector_column ${vector_column} --output_folder ${output_dir} --output_description train --subject_col_name repertoire.repertoire_name --labels_col_name repertoire.disease_diagnosis --cpus=12 2>&1 | tee ${output_dir}/build_cluster_proximity_feature_table_with_labels.log.txt
+				fi
+				if [ -f ${output_dir}/test_feature_table.csv ] ; then
+					echo "${output_dir}/test_feature_table.csv already exists, skipping building test feature table."	
+				else
+					echo "Building test feature table..."
+					# create test feature table
+					python -u ~/antibody_sequence_embedding/executable_scripts/build_cluster_proximty_feature_table_with_labels.py --features_list ${output_dir}/feature_list.csv --data_file_path ${fold_dir}/*_TEST_*.tsv --vector_column ${vector_column} --output_folder ${output_dir} --output_description test --subject_col_name repertoire.repertoire_name --labels_col_name repertoire.disease_diagnosis --cpus=12 2>&1 | tee -a ${output_dir}/build_cluster_proximity_feature_table_with_labels.log.txt
+
+				fi
+			done # min subjects loop
+		done # significance level loop
+	done # cluster size loop
+done # fold loop
---
+++ scripts/pre_clustering_pipline.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+trap "exit" INT
+
+trim_start=2
+trim_end=1
+query=""
+sample_size=10000
+description=""
+model_file=""
+random_seed=0
+work_dir=./
+usage="Usage - pre_clustering_pipeline.sh -d <description> -q [query] -s [sample_size] -t [trim_start:trim_end] -m [model_file] -r [random_seed] -n[num of folds] -w [work_dir]"
+help="Execute all the pre processing steps before clustring a data set.
+-d DESCRIPTION - Mandatory, decription of which will be used as suffix for the resulting files and added columns. For example \"-d celiac_heavy_chain\".
+-q QUERY - A KQL query to import data from the elastic search. Mandatory if <decsription>.tsv file doesn't exist in input direction
+-t TRIM_START:TRIM_END - Optional, Amino Acids to trim from the start of the junction_aa. trim_start - Amino Acids to trim from the end for the junction_add. Default is 1.
+-m MODEL_FILE - Optional, model file to be used to embed the junction_aa, if not provided a model will be generated from data set
+-s SAMPLE_SIZE - Optional, number of sequences to sample from each subject repertoire for the clustering. Default is 10000
+-r RANDOM_SEED - Optional, seed for the sampling. Default is 0.
+-n N_FOLDS - Optional, number of folds for the repeated cross validation splitting. Deafult is 10.
+-w WORK_DIR - Optional, the work directory. Deafult is \"./\"
+"
+
+while getopts "hq:s:t:d:m:r:f:w:" opt; do
+	case ${opt} in
+		h ) echo "${usage}" ; echo "${help}"; exit 1
+      			;;
+    		q ) query=${OPTARG}
+		     	;;
+		s ) sample_size=${OPTARG}
+			;;
+		t ) trim_start=$(echo ${OPTARG} | awk -F $':' '{print $1}'); trim_end=$(echo ${OPTARG} | awk -F $':' '{print $2}')
+			;;
+		d ) description=${OPTARG}
+			;;
+		m ) model_file=${OPTARG}
+			;;
+		r ) random_seed=${OPTARG}
+			;;
+		f ) n_folds=${OPTARG}
+			;;
+		w ) input_dir=${OPTARG}
+			;;
+		\? ) echo "${usage}"; exit 1
+      			;;
+	esac
+done
+
+# change dirctory to input directory
+cd ${work_dir}
+
+# arguments validation
+if [ -z "${description}" ]; then
+	echo "Missing mandatory description argument. Exiting..."
+	echo "${usage}"
+	echo "pre_clustering_pipeline.sh -h for additional help."
+	exit -1
+fi
+
+if [ -z "${model_file}" ]; then
+	model_desc=${description}_trim_${trim_start}_${trim_end}_prot2vec
+else
+	model_desc=$(echo ${model_file} | awk -F $'.' '{print $1}')
+fi
+
+# import file from elastic search data base
+import_file=${description}.tsv
+if [ -f ${import_file} ]; then
+	echo "File ${import_file} already exists, skipping data import."; echo ""
+else
+	if [ -z "${query}" ]; then
+		echo "Missing mandatory description argument. Exiting..."
+		echo "${usage}"
+		echo "pre_clustering_pipeline.sh -h for additional help."
+		exit -1
+	fi
+	
+	echo "Importing data..."
+	python /work/data/es_lab/import_data.py --index sequence_v1 --query ${query} --include_fields "junction;junction_length;productive;consensus_count;junction_aa;v_call;${model_desc};repertoire.subject_id;repertoire.repertoire_name;repertoire.disease_diagnosis" --count_only False --output_file ${import_file} --max_records 4000000
+fi
+
+# junction might be already embedded
+if ! [ -z "$(head -n1 ${import_file} | grep ${model_desc})" ]; then
+	echo "Column ${model_desc} already exists in ${import_file}, skipping reprocess mkdb, generate model and generate vectors."	
+	mkdb_file=${import_file}
+else
+	# Reprocess mkdb
+	mkdb_file=${description}_trim_${trim_start}_${trim_end}.tsv
+	if [ -f ${mkdb_file} ]; then
+		echo "File ${mkdb_file} already exists, skipping reprocess mkdb."; echo ""
+	else
+		echo "Reprocess mkdb..."; echo ""
+		python ~/antibody_sequence_embedding/executable_scripts/reprocess_makedb_dir.py --input_files ${import_file} --output_file ${mkdb_file} --trim 2 1 --min_seq_per_subject ${sample_size}
+	fi
+	
+	if ! [ -z "$(head -n1 ${mkdb_file} | grep ${model_desc})" ]; then
+		echo "Column ${model_desc} already exists in ${mkdb_file}, skipping generate model and generate vectors."; echo ""
+	else
+		if ! [ -z "${model_file}" ]; then
+			# model file was already provided
+			if ! [ -f ${model_file} ]; then
+				echo "Bad path for model file: ${model_file}. Exiting..."
+				echo "${usage}"
+				echo "pre_clustering_pipeline.sh -h for additional help."
+				exit -1
+			else
+				echo "Model file ${model_file} provided, skipping generate model."; echo ""
+			fi
+		else
+			# generate model
+			model_file=${model_desc}.model
+			if [ -f ${model_file} ]; then
+				echo "Model file ${model_file} already exists, skipping generate model."; echo ""
+			else
+				echo "Generate model..."; echo ""
+				python ~/antibody_sequence_embedding/executable_scripts/generate_model.py --data_file ${mkdb_file} --data_column junction_aa_trim_${trim_start}_${trim_end} --desc ${model_desc} --n_gram 3
+			fi
+		fi
+		
+		# generate vectors
+		echo "Generate vectors..."; echo ""	
+		python ~/antibody_sequence_embedding/executable_scripts/generate_vectors.py ${model_file} --input_file ${mkdb_file} --column junction_aa_trim_${trim_start}_${trim_end} --output_column ${model_desc} --inline True --size 100
+	fi
+fi
+
+# sample file
+sampled_file=${description}_trim_${trim_start}_${trim_end}_sampled_n${sample_size}_seed${random_seed}.tsv
+if [ -f ${sampled_file} ]; then
+	echo "File ${sampled_file} already exists, skipping sampling."; echo ""
+else
+	echo "Sample file..."; echo ""
+	python ~/antibody_sequence_embedding/executable_scripts/sample_file.py --input_data_file ${mkdb_file} --exclude_dup_column junction_aa_trim_${trim_start}_${trim_end} --min_samples ${sample_size}
+fi
+
+# create repeated cross validation folds
+folds_dir=${description}_trim_${trim_start}_${trim_end}_sampled_n${sample_size}_seed${random_seed}_${n_folds}folds
+if [ -d ${folds_dir} ]; then
+	echo "Folds dir ${folds_dir} already exists, skipping folds creation."; echo ""
+else
+	echo "Split folds..."; echo ""
+	python ~/antibody_sequence_embedding/executable_scripts/split_data_train_test_folds.py ${sampled_file} --test_fraction "0.1" --repeated=True --output_dir ${folds_dir} --number_of_folds ${n_folds}
+fi
+
