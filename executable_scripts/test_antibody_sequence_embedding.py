79e564df6567807426a8f00726a3cdfad4a77ad7
Boaz Frankel <>
2020-11-01 11:54:25 +0200

    test file generator

--- executable_scripts/test_antibody_sequence_embedding.py
+++ executable_scripts/test_antibody_sequence_embedding.py
@@ -10,6 +10,7 @@ def main():
     parser.add_argument('--subj_sample_size', help='', default=100)
     parser.add_argument('--embedding_dim', help='', default=100)
     parser.add_argument('--output_file', help='test.tsv')
+    execute()
 
 
 def execute(args):
