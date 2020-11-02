d5cc94e8c6f429f581aa152783f794a27d9cf391
boazfr@gmail.com <boazfr@bcr.lnx.biu.ac.il>
2020-11-02 14:32:50 +0200

    fix syntax errors

--- executable_scripts/shuffle_labels.py
+++ executable_scripts/shuffle_labels.py
@@ -6,29 +6,29 @@ import os
 
 
 def main():
-	parser = argparse.ArgumentParser(description='')
-	parser.add_argument('--input_file', help='')
-	parser.add_argument('--output_file', help='')
-	parser.add_argument('--label_col', help='')
-	parser.add_argument('--id_col', help='')
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--input_file', help='')
+    parser.add_argument('--output_file', help='')
+    parser.add_argument('--label_col', help='')
+    parser.add_argument('--id_col', help='')
 
-	args = parser.parse_args()
-	execute(args)
+    args = parser.parse_args()
+    execute(args)
 
 
 def execute(args):
 
-	df = pd.read_csv(args.input_file, sep='\t')
-	subjects = pd.DataFrame()
-	subjects['id'] = df[args.id_col].unique().tolist()
-	subjects['label'] = [df.loc[df[args.id_col] == x, args.label_col][0] for x in subjects['id']]
-	subjects['label'] = np.random.permutation(subjects['label'].values)
+    df = pd.read_csv(args.input_file, sep='\t')
+    subjects = pd.DataFrame()
+    subjects['id'] = df[args.id_col].unique().tolist()
+    subjects['label'] = [df.loc[df[args.id_col] == x, args.label_col].iloc[0] for x in subjects['id'].to_list()]
+    subjects['label'] = np.random.permutation(subjects['label'].values)
 
-	for idx, row in subjects.iterrows():
-		df.loc[df[args.id_col] == row['id'], args.label_col] = row['label']
+    for idx, row in subjects.iterrows():
+        df.loc[df[args.id_col] == row['id'], args.label_col] = row['label']
 
-	df.to_csv(args.output_file, '\t')
+    df.to_csv(args.output_file, '\t')
 
 
 if __name__ == '__main__':
-	main()
+    main()
