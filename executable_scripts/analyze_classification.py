e2fcaf627229691da359f8ad2963bad6efb240a3
boazfr@gmail.com <boazfr@ig02.lnx.biu.ac.il>
2020-11-07 21:55:44 +0200

    change to generic columns names

--- executable_scripts/analyze_classification.py
+++ executable_scripts/analyze_classification.py
@@ -7,7 +7,6 @@ import numpy as np
 import json
 import ast
 
-
 def group_by_metric(df: pd.DataFrame, metric_list: list) -> pd.DataFrame:
 
     if len(metric_list) == 0:
@@ -49,8 +48,10 @@ def main():
     parser.add_argument('--input_file', help='input classification results csv file')
     parser.add_argument('--processed_input_file', help='already processed classification results csv file')
     parser.add_argument('--group_by', help='comma separated list of columns based on do the analysis', type=str,
-                        default="cluster_size,significance,min_subj,model,report,key")
+                        default="cluster_size,max_features,model,report,key")
     parser.add_argument('--model', help='Run analysis for a specific model', type=str)
+    parser.add_argument('--numeric_hyper_params', help='Semicolon separated list of Numeric generic hyper parameters '
+                                                       'in csv file', type=str)
     parser.add_argument('--min_folds', help='min number of folds for analysis and ranking. By default will be the '
                                             'max number of folds', type=str)
     parser.add_argument('--output_dir', help='output_dir to save analysis output files. Default: ./analysis', type=str,
@@ -99,34 +100,38 @@ def execute(args):
     if min_folds is None:
         min_folds = int(len(input_file['fold'].unique()) * 0.7)
 
+    if args.numeric_hyper_params is not None:
+        numeric_hyper_params = args.numeric_hyper_params.split(';')
+    else:
+        numeric_hyper_params = []
+
     # filter only test reports
     df_filtered = df.loc[(df['report'] == 'test') & (df['n_folds'] >= min_folds) & (df['key'] == 'macro avg'), : ]
 
     df_filtered = df_filtered.sort_values(by='f1_score_mean', ascending=False);
-    df_filtered.to_csv(os.path.join(output_dir, "accumlated_" + args.input_file), index=False)
+    df_filtered.to_csv(os.path.join(output_dir, "accumulated_" + args.input_file), index=False)
 
-    top_10 = df_filtered.head(10).loc[:, ['model', 'cluster_size', 'significance', 'min_subj', 'n_features_mean', 'f1_score_mean', 'n_folds']]
-    top_10 = top_10.copy(deep=True)
-    top_10['f1_score_mean'] = top_10['f1_score_mean'].apply(lambda x: round(x, 2))
+    models_table_columns = ['model', 'n_folds', 'cluster_size', 'max_features', 'n_features_mean', 'f1_score_mean']
+
+    models_list = [df_filtered[df_filtered['model'] == model].iloc[0, :].loc[models_table_columns] for
+                   model in df_filtered['model'].unique()]
+    models_df = pd.concat(models_list, axis=1).transpose()
+    models_df['f1_score_mean'] = models_df['f1_score_mean'].apply(lambda x: round(x, 2))
+    print(models_df)
 
     plt.clf()
     plt.figure()
-    plt.table(cellText=top_10.to_numpy(), colLabels=top_10.columns, loc='center')
+    plt.table(cellText=models_df.to_numpy(), colLabels=models_df.columns, loc='center')
     plt.axis('off')
     plt.savefig(os.path.join(output_dir,  "top_models.png"), dpi=1200)
-
     plt.clf()
-    sns.set(rc={'figure.figsize': (18, 10)})
-    fig, axs = plt.subplots(ncols=2, nrows=2)
 
     best_model = df_filtered.iloc[0, df_filtered.columns == 'model'][0]
-    print("best model is {} with f1_score_mean {}".format(best_model,
-                                                       df_filtered.iloc[0, df_filtered.columns == 'f1_score_mean'][0]))
     best_model_df = df_filtered.loc[df_filtered['model'] == best_model, :]
     best_model_df = best_model_df.sort_values(by='f1_score_mean', ascending=False);
     best_model_df.reset_index(inplace=True)
 
-    chart = sns.lineplot(data=best_model_df, x="n_features_mean", y="f1_score_mean", ax=axs[0, 0])
+    chart = sns.lineplot(data=best_model_df, x="n_features_mean", y="f1_score_mean")
     ticks = np.arange(int(best_model_df['n_features_mean'].min()),
                       int((best_model_df[ 'n_features_mean'].max() - best_model_df['n_features_mean'].min()) / 10),
                       int(best_model_df['n_features_mean'].max()))
@@ -134,32 +139,7 @@ def execute(args):
     chart.set_xticklabels(ticks, rotation=90)
     chart.set_title('Number of features vs f1 score')
 
-    best_cluster_size = best_model_df.iloc[0, df_filtered.columns == 'cluster_size'][0]
-    best_min_subj = best_model_df.iloc[0, df_filtered.columns == 'min_subj'][0]
-    best_significance = best_model_df.iloc[0, df_filtered.columns == 'significance'][0]
-
-    chart = sns.lineplot(data=best_model_df[(best_model_df["min_subj"] == best_min_subj) &
-                                            (best_model_df["significance"] == best_significance)],
-                         x="cluster_size",
-                         y="f1_score_mean",
-                         ax=axs[0, 1])
-    chart.set_title('Cluster size vs f1 score')
-
-    chart = sns.lineplot(data=best_model_df[(best_model_df["min_subj"] == best_min_subj) &
-                                            (best_model_df["cluster_size"] == best_cluster_size)],
-                         x="significance",
-                         y="f1_score_mean",
-                         ax=axs[1, 0])
-    chart.set_title('Significance vs f1 score')
-
-    chart = sns.lineplot(data=best_model_df[(best_model_df["cluster_size"] == best_cluster_size) &
-                                            (best_model_df["significance"] == best_significance)],
-                         x="min_subj",
-                         y="f1_score_mean",
-                         ax=axs[1, 1])
-    chart.set_title('Min subjects vs f1 score')
-
-    fig.savefig(os.path.join(output_dir, best_model + "_cluster_parameters_vs_f1_score.png"))
+    chart.get_figure().savefig(os.path.join(output_dir, best_model + "_cluster_parameters_vs_f1_score.png"))
     plt.clf()
 
     if 'confusion_matrix' in best_model_df.columns:
@@ -174,33 +154,17 @@ def execute(args):
         plt.savefig(os.path.join(output_dir, best_model + "_confusion_matrix.png"))
         plt.clf()
 
-    indexing = (input_file['cluster_size'] == best_cluster_size) & (input_file['min_subj'] == best_min_subj) & \
-               (input_file['significance'] == best_significance) & (input_file['model'] == best_model) & \
-               (input_file['report'] == 'test') & (input_file['key'] == 'macro avg')
-    chart = sns.distplot(a=input_file.loc[indexing, 'f1_score'], hist=True)
+    df = input_file[(input_file['model'] == best_model) & (input_file['report'] == 'test') & (input_file['key'] == 'macro avg')]
+    for param in numeric_hyper_params:
+        df = df[param == best_model_df.at[best_model_df.index[0], param]]
+
+    chart = sns.distplot(a=df['f1_score'], hist=True)
     chart.set_title(best_model + '_f1 score histogram')
     chart.get_figure().savefig(os.path.join(output_dir, best_model + '_f1_score_hist.png'))
     plt.clf()
-    
-    best_models = df_filtered['model'].unique()[0:min(4, len(df_filtered['model'].unique()))]
-    best_models_df_list = []
-    for model in best_models:
-        model_df = df_filtered.loc[df_filtered['model'] == model, : ]
-        model_df = model_df.sort_values(by='f1_score_mean', ascending=False);
-        model_cluster_size = model_df.iloc[0, df_filtered.columns == 'cluster_size'][0]
-        model_min_subj= model_df.iloc[0, df_filtered.columns == 'min_subj'][0]
-        model_significance= model_df.iloc[0, df_filtered.columns == 'significance'][0]
-        indexing = (input_file['cluster_size'] == model_cluster_size) & (input_file['min_subj'] == model_min_subj) & \
-                   (input_file['significance'] == model_significance) & (input_file['model'] == model) & \
-                   (input_file['report'] == 'test') & (input_file['key'] == 'macro avg')
-        best_models_df_list += [input_file.loc[ indexing, : ]]
-    best_models_df = pd.concat(best_models_df_list)
-    chart = sns.scatterplot(data=best_models_df, x="fold", y="f1_score", hue="model", x_jitter=0.2, y_jitter=0.2)
-    chart.set_xticks(best_models_df['fold'].unique())
-    chart.set_xticklabels(best_models_df['fold'].unique())
-    chart.get_figure().savefig(os.path.join(output_dir, "f1_score_by_folds.png".format(len(best_models))))
 
 
 if __name__ == "__main__":
     main()
+
 
