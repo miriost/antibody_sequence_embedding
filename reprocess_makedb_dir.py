a6f4ddf98f407eb2ce01d3bc588dfca899104099
Boaz Frankel <>
2020-11-29 02:23:13 +0200

    distance from germline filter for naive data sets

--- reprocess_makedb_dir.py
+++ reprocess_makedb_dir.py
@@ -10,71 +10,79 @@ Created on Mon Nov  5 11:46:26 2018
 import sys, argparse
 import os
 import pandas as pd
+from Levenshtein import distance
 
 sys.path.insert(0, os.path.join(os.pardir, os.path.pardir))
 
 
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1', "True"):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0', "False"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_files', help='a space separated list of repertoire tsv files '
-                                              'for db creation', nargs='+')
-    parser.add_argument('--output_file', help='output file', type=str)
-    parser.add_argument('--subject_field', help='output file', type=str, default='repertoire.repertoire_name')
+    parser.add_argument('input_file', help='input tsv file')
+    parser.add_argument('output_file', help='output file', type=str)
+    parser.add_argument('--subject_field', help='output file', type=str, default='subject.subject_id')
     parser.add_argument('--trim', nargs=2, type=int, help='two integers to trim, first from beginning, 2nd from end')
-    parser.add_argument('--min_seq_per_subject', type=int, help='min number of sequence per subject', default=10000)
+    parser.add_argument('--naive', nargs=2, type=str2bool, help='sequences are of naive cells. default is False.',
+                        default=False)
+    parser.add_argument('--min_seq_per_subject', type=int, help='min number of sequence per subject', default=2000)
 
     args = parser.parse_args()
     total_rows = 0
+    input_file = args.input_file
 
     df_list = []
 
-    for file in args.input_files:
-        if not (os.path.isfile(file)):
-            print('cannot open {}.\nExiting...'.format(file))
-            sys.exit(1)
+    if not (os.path.isfile(input_file)):
+        print('cannot open {}.\nExiting...'.format(input_file))
+        sys.exit(1)
 
-        file_df = pd.read_csv(file, sep='\t')
+    file_df = pd.read_csv(input_file, sep='\t')
+    file_df['cdr3_length'] = file_df['cdr3'].str.len()
+    file_df['cdr3_aa_length'] = file_df['cdr3_aa'].str.len()
 
-        by_subject = file_df.groupby([args.subject_field])
+    by_subject = file_df.groupby([args.subject_field])
 
-        for subject, frame in by_subject:
-        
-            total_rows += len(frame)
+    for subject, frame in by_subject:
 
-            local_df = frame
+        total_rows += len(frame)
 
-            print('file {} sujbect {} total rows: {}'.format(file, subject, len(local_df)))
+        local_df = frame
 
-            # 1. REMOVE NON-FUNCTIONAL SEQUENCES
-            local_df = local_df.loc[local_df.productive == True, :]
-            print(' - After removing non functional sequences: {}'.format(len(local_df)))
+        print('file {} sujbect {} total rows: {}'.format(file, subject, len(local_df)))
 
-            # 2. REMOVE ROWS WHERE JUNCTION LENGTH IS SHORTER THAN 12 OR DOESN'T DEVIDE BY 3
-            local_df = local_df[local_df.junction_length > 12]
-            local_df = local_df[local_df.junction_length % 3 == 0]
-            print(' - After junction_length>12, junction_length % 3 == 0 : {}'.format(len(local_df)))
+        # 1. REMOVE NON-FUNCTIONAL SEQUENCES
+        local_df = local_df.loc[local_df.productive == True, :]
+        print(' - After removing non functional sequences: {}'.format(len(local_df)))
 
-            # 3. LEAVE ONLY ROWS WHERE CONSCOUNT > 1
-            local_df = local_df[local_df['consensus_count'] > 1]
-            print(' - After consensus_count > 1: {} '.format(len(local_df)))
+        # 2. REMOVE ROWS WHERE CDR3 LENGTH DOESN'T DIVIDE BY 3
+        local_df = local_df[local_df.cdr3_length % 3 == 0]
+        print(' - After cdr3_length % 3 == 0 : {}'.format(len(local_df)))
 
-            # 4. REMOVE SEQUENCES WITH 'N' OR '-']
-            local_df = local_df[local_df.junction.str.contains('N') == False]
-            local_df = local_df[local_df.junction.str.contains('-') == False]
-            print(' - After removing sequences with N or gaps: {}'.format(len(local_df)))
+        # 3. LEAVE ONLY ROWS WHERE CONSCOUNT > 1
+        local_df = local_df[local_df['consensus_count'] > 1]
+        print(' - After consensus_count > 1: {} '.format(len(local_df)))
 
-            # 5. trimming
-            if args.trim:
-                local_df = local_df[local_df.junction.str.len() - args.trim[0] - args.trim[1] >=3]
-                print('trimming {} AAs from he beginning, {} AAs from to end'.format(args.trim[0], args.trim[1]))
-                local_df['junction_aa_trim_' + str(args.trim[0]) + '_' + str(args.trim[1])] = \
-                        local_df.junction_aa.str[args.trim[0]:-args.trim[1]]
-                print(' - After trimming: {}'.format(len(local_df)))
+        # 4. REMOVE SEQUENCES WITH EDIT DISTANCE > 3 FROM GERMLINE
+        if args.naive:
+            dist_from_germline = local_df.apply(lambda x: distance(x['v_germline_alignment'],
+                                                                   x['v_sequence_alignment']), axis=1)
+            local_df = local_df[dist_from_germline <= 3]
+            print(' - After dist_from_germline <= 3: {} '.format(len(local_df)))
 
-            if len(local_df) >= args.min_seq_per_subject:
-                df_list += [local_df]
-            else:
-                print('subject not added due to low number of sequences')
+        if len(local_df) >= args.min_seq_per_subject:
+            df_list += [local_df]
+        else:
+            print('subject not added due to low number of sequences')
 
     if len(df_list) == 0:
         print("zero sequences where filtered")
