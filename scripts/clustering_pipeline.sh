ff29a88d69cebba7f6d333678dfbf77da3b51fca
Boaz Frankel <>
2020-12-21 00:15:20 +0200

    r

--- scripts/clustering_pipeline.sh
+++ scripts/clustering_pipeline.sh
@@ -142,11 +142,10 @@ for fold in ${folds} ; do
 
           # build feature list
           echo "nice -19 python -u ~/antibody_sequence_embedding/build_knn_cluster_proximity_feature_list.py ${fold_dir}/${data_file} ${fold_dir}/${vectors_file} ${knn_dir}/${knn_itr}knn_distances.npy
-          ${knn_dir}/${knn_itr}knn_neighbors.npy ${max_distance_percentil_dir}
-          100knn_${p}p_feature_list --min_subjects ${min_subjects_itr} --min_significant ${min_significant_itr} --max_distance_percentil ${max_distance_percentil_itr}"
-          nice -19 python -u ~/antibody_sequence_embedding/build_knn_cluster_proximity_feature_list.py ${fold_dir}/${data_file} ${fold_dir}/${vectors_file} ${knn_dir}/${knn_itr}knn_distances.npy
-          ${knn_dir}/${knn_itr}knn_neighbors.npy ${max_distance_percentil_dir}
-          100knn_${p}p_feature_list --min_subjects ${min_subjects_itr} --min_significant ${min_significant_itr} --max_distance_percentil ${max_distance_percentil_itr}
+          ${knn_dir}/${knn_itr}knn_neighbors.npy ${max_distance_percentil_dir}"
+          nice -19 python -u ~/antibody_sequence_embedding/build_knn_cluster_proximity_feature_list.py ${fold_dir}/${data_file} ${fold_dir}/${vectors_file} ${knn_dir}/${knn_itr}knn_distances.npy 
+          ${knn_dir}/${knn_itr}knn_neighbors.npy ${max_distance_percentil_dir} 100knn_${p}p_feature_list --min_subjects ${min_subjects_itr} --min_significant ${min_significant_itr}
+          --max_distance_percentil ${max_distance_percentil_itr}
 
         done # max_distance_percentil loop
       done # min_significant loop
