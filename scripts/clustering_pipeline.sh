83b654f0c56c8fe707ead5d72ed75492ac579f28
Boaz Frankel <>
2020-12-21 00:29:11 +0200

    r

--- scripts/clustering_pipeline.sh
+++ scripts/clustering_pipeline.sh
@@ -7,14 +7,14 @@ function show_help {
   echo "folds FOLDS - Optional, space separated list of folds numbers. Deafult is 0."
   echo "knn KNN - Optional, space separated list of the K nearest neighbors to serach in the clusters construction. Deafult is 100."
   echo "max_distance_percentil MAX_DISTANCE_percentil - Optional, space separated list of max distance pecentile for filtering cluster neighbors. Default is \"100\" (all knn neighbors)."
-  echo "min_significant MIN_SIGNIFICANT - Optional, space separated list of minimal significant threshould for the cluster selection. Default is \"0.7\"."
+  echo "min_significance MIN_significance - Optional, space separated list of minimal significance threshould for the cluster selection. Default is \"0.7\"."
   echo "min_subjects MIN_SUBJECTS - Optional, a space separated list of the number minimal of subjects threshold for the cluster selection. Default is \"7\"."
   echo "work_dir WORK_DIR - Optional, the folds root directory where the folds are. Default is \"./\"."
 }
 
 folds=0
 knn=100
-min_significant="0.7"
+min_significance="0.7"
 work_dir=./
 min_subjects=7
 max_distance_percentil=100
@@ -27,7 +27,7 @@ ARGUMENT_LIST=(
     "folds"
     "knn"
     "max_distance_percentil"
-    "min_significant"
+    "min_significance"
     "min_subjects"
     "work_dir"
 )
@@ -64,8 +64,8 @@ while [[ $# -gt 0 ]]; do
         max_distance_percentil=$2
         shift 2
         ;;
-      --min_significant)
-        min_significant=$2
+      --min_significance)
+        min_significance=$2
         shift 2
         ;;
       --min_subjects)
@@ -123,16 +123,16 @@ for fold in ${folds} ; do
 			min_subjects_dir=${knn_dir}/min_subjects_${min_subjects_itr}
 			mkdir -p ${min_subjects_dir}
 
-      #loop min significant
-      for min_significant_itr in ${min_significant}; do
-        echo "min_significant ${min_significant_itr}"; echo ""
-        min_significant_dir=${min_subjects_dir}/min_significant_${min_significant_itr}
-        mkdir -p ${min_significant_dir}
+      #loop min significance
+      for min_significance_itr in ${min_significance}; do
+        echo "min_significance ${min_significance_itr}"; echo ""
+        min_significance_dir=${min_subjects_dir}/min_significance_${min_significance_itr}
+        mkdir -p ${min_significance_dir}
 
         #loop max_distance_percentil
         for max_distance_percentil_itr in ${max_distance_percentil}; do
           echo "max_distance_percentil ${max_distance_percentil_itr}"; echo ""
-          max_distance_percentil_dir=${min_significant_dir}/max_distance_percentil_${max_distance_percentil_itr}
+          max_distance_percentil_dir=${min_significance_dir}/max_distance_percentil_${max_distance_percentil_itr}
           mkdir -p ${max_distance_percentil_dir}
 
           if [ -f ${max_distance_percentil_dir}/festure_list.tsv ] ; then
@@ -143,12 +143,12 @@ for fold in ${folds} ; do
           # build feature list
           cmd="nice -19 python -u ~/antibody_sequence_embedding/build_knn_cluster_proximity_feature_list.py${fold_dir}/${data_file} ${fold_dir}/${vectors_file} ${knn_dir}/${knn_itr}\knn_distances
           .npy ${knn_dir}/${knn_itr}knn_neighbors.npy ${max_distance_percentil_dir} 100knn_${p}p_feature_list --min_subjects ${min_subjects_itr} --min_significance
-          ${min_significant_itr}\ --max_distance_percentil ${max_distance_percentil_itr}"
+          ${min_significance_itr}\ --max_distance_percentil ${max_distance_percentil_itr}"
           echo ${cmd}
           eval ${cmd}
 
         done # max_distance_percentil loop
-      done # min_significant loop
+      done # min_significance loop
 		done # min_subjects loop
 	done # knnloop
 done # fold loop
