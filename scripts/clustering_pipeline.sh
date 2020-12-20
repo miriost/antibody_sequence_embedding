36fa0380c96fc4fec348425f269ea6017016de91
Boaz Frankel <>
2020-12-21 00:34:25 +0200

    r

--- scripts/clustering_pipeline.sh
+++ scripts/clustering_pipeline.sh
@@ -142,9 +142,9 @@ for fold in ${folds} ; do
           fi
 
           # build feature list
-          cmd="nice -19 python -u ~/antibody_sequence_embedding/build_knn_cluster_proximity_feature_list.py ${fold_dir}/${data_file} ${fold_dir}/${vectors_file} ${knn_dir}/${knn_itr}\knn_distances
-          .npy ${knn_dir}/${knn_itr}knn_neighbors.npy ${max_distance_percentil_dir} 100knn_${p}p_feature_list --min_subjects ${min_subjects_itr} --min_significance
-          \"${min_significance_itr}\"\ --max_distance_percentil ${max_distance_percentil_itr}"
+          cmd="nice -19 python -u ~/antibody_sequence_embedding/build_knn_cluster_proximity_feature_list.py ${fold_dir}/${data_file} ${fold_dir}/${vectors_file} ${knn_dir}/${knn_itr}knn_distances
+          .npy ${knn_dir}/${knn_itr}knn_neighbors.npy ${max_distance_percentil_dir} 100knn_${p}p_feature_list --min_subjects ${min_subjects_itr} --min_significance ${min_significance_itr}
+          --max_distance_percentil ${max_distance_percentil_itr}"
           echo ${cmd}
           eval ${cmd}
 
